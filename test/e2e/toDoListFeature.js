0d15176733434875d389ad849b34b2a097e5eaae
Hamid Zade <hamidmamadzade@gmail.com>
2016-02-01 01:13:32 +0000

    adds some css (incomplete)

--- test/e2e/toDoListFeature.js
+++ test/e2e/toDoListFeature.js
@@ -1,7 +1,24 @@
 describe("ToDo List", function() {
-  it("has a title", function() {
-    browser.get("http://localhost:8080");
+  var taskfield = element(by.model("addCtrl.task"));
+  var addButton = element(by.className("addBtn"));
+  var taskList = element(by.className("taskList"));
 
+  beforeEach(function() {
+    browser.get("http://localhost:8080");
+  });
+
+  it("has a title", function() {
     expect(browser.getTitle()).toEqual("To Do List");
   });
+
+  it("adding toDo's on the page", function() {
+    taskfield.sendKeys("enter batcave");
+    addButton.click();
+    expect(taskList.getText()).toEqual("enter batcave edit");
+  });
+
+  it("can clear completed", function() {
+    
+  });
+
 });
